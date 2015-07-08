39afd6497b5ba7d03fff9edba94b74dded232683
Jonatan <jrperdomoz@gmail.com>
2015-07-08 03:32:35 -0700

    update project

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# django-khipu
-
+# pykhipu
---
+++ khipu/__init__.py
@@ -0,0 +1,28 @@
+import hashlib
+import hmac
+import requests
+
+from . import version
+
+__version__ = version.__version__
+
+
+class KhipuAPI(object):
+    """
+    A client for the Khipu API.
+    """
+    base_url = 'https://khipu.com/api/1.3/'
+    
+    def __init__(self, receiver_id=None, secret=None):
+        self.receiver_id = receiver_id
+        self.secret = secret
+        self._hash = hmac.new(
+            secret, "receiver_id=" + receiver_id, hashlib.sha256
+        ).hexdigest()
+        self.data = {
+            'receiver_id': receiver_id,
+            'hash': self._hash
+        }
+        
+    def request(self, path):
+        return requests.post(self.base_url + path, data=self.data)
