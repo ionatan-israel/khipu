7678352beedc1478d1539cd7d2f1a3c305c26294
Jonatan <jrperdomoz@gmail.com>
2015-07-09 02:53:31 -0700

    Creo la clase Khipu que Provee y centraliza la carga de los servicios que presta Khipu

--- khipu/__init__.py
+++ khipu/__init__.py
@@ -1,24 +1,69 @@
-import hashlib
-import hmac
-import requests
+# -*- coding: utf-8 -*-
+
+import imp
+import os
+import services
 
 
-class KhipuAPI(object):
+# Definimos la ruta de Khipu
+# KHIPU_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Khipu(object):
     """
-    A client for the Khipu API.
+    Provee y centraliza la carga de los servicios que presta Khipu
     """
-    base_url = 'https://khipu.com/api/1.3/'
+    # Corresponde al ID del cobrador.
+    receiver_id = None
+    # Corresponde a la llave del cobrador.
+    secret = None
     
-    def __init__(self, receiver_id=None, secret=None):
+    # diccionario que contiene los nombres de los servicios que existen
+    # y si requieren autenticación
+    services = {
+        'CreateEmail': True,
+        'CreatePaymentPage': True,
+        'CreatePaymentURL': True,
+        'VerifyPaymentNotification': False,
+        'ReceiverStatus': True,
+        'SetBillExpired': True,
+        'SetPaidByReceiver': True,
+        'SetRejectedByPayer': True,
+        'PaymentStatus': True,
+        'UpdatePaymentNotificationUrl': True,
+        'ReceiverBanks': True,
+        'GetPaymentNotification': True
+    }
+    
+    def authenticate(self, receiver_id, secret):
+        """
+        Identificar al cobrador que utilizara los servicios.
+        No es necesario para utilizar el servicio VerifyPaymentNotification.
+        """
         self.receiver_id = receiver_id
         self.secret = secret
-        self._hash = hmac.new(
-            secret, "receiver_id=" + receiver_id, hashlib.sha256
-        ).hexdigest()
-        self.data = {
-            'receiver_id': receiver_id,
-            'hash': self._hash
-        }
-        
-    def request(self, path):
-        return requests.post(self.base_url + path, data=self.data)
+
+    def load_service(self, service_name):
+        """
+        Carga el servicio y retorna el objecto, en caso de no existir
+        el servicio, se invoca una excepcion
+        """
+        if service_name in self.services:
+            # Es requerido identificarse para usar estos servicios
+            if self.receiver_id and self.secret:
+                class_name = 'KhipuService' + service_name
+                return getattr(services, class_name)(self.receiver_id, self.secret, service_name)
+            else:
+                raise KhipuError(
+                    "Is necessary to authenticate to use the service {0}".format(
+                        service_name))
+        else:
+            # Invocamos un Exception
+            raise KhipuError(
+                "The service {0} does not exist".format(service_name))
+
+
+class KhipuError(Exception):
+    
+    def __init__(self, result):
+        Exception.__init__(self, result)
