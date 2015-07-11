3d1627bf207e514b7caf52dabbe4d7b0fb3b8fa2
Jonatan <jrperdomoz@gmail.com>
2015-07-10 22:45:39 -0700

    Add service receiverBanks

---
+++ khipu/services/receiver_banks.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from common import KhipuService
+
+
+class KhipuServiceReceiverBanks(KhipuService):
+    
+    def __init__(self, receiver_id, secret, service_name, **kwargs):
+        super(
+            KhipuServiceReceiverBanks, self).__init__(
+                receiver_id, secret, service_name, **kwargs)
+        self.data = {'receiver_id': receiver_id}
