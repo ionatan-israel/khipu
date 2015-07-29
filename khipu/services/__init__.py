f34386c62a3fcedcc8a65181ebb270dfc3003c72
Jonatan <jrperdomoz@gmail.com>
2015-07-28 23:52:36 -0700

    Agrego la clase KhipuServiceCreatePaymentURL para hacer peticiones al servicio CreatePaymentURL

--- khipu/services/__init__.py
+++ khipu/services/__init__.py
@@ -1,3 +1,4 @@
 from common import KhipuService
 from receiver_status import KhipuServiceReceiverStatus
 from receiver_banks import KhipuServiceReceiverBanks
+from create_payment_url import KhipuServiceCreatePaymentURL
